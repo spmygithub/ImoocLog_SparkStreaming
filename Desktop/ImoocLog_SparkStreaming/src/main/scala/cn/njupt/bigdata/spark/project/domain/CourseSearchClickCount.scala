7f9aeeb7f5734d1f1337ec975a09f9732e87fcda
spmygithub <33421631+spmygithub@users.noreply.github.com>
2018-09-17 16:07:12 +0800

    Update CourseSearchClickCount.scala

--- Desktop/ImoocLog_SparkStreaming/src/main/scala/cn/njupt/bigdata/spark/project/domain/CourseSearchClickCount.scala
+++ Desktop/ImoocLog_SparkStreaming/src/main/scala/cn/njupt/bigdata/spark/project/domain/CourseSearchClickCount.scala
@@ -1,5 +1,8 @@
 package cn.njupt.bigdata.spark.project.domain
 
-class CourseSearchClickCount {
-
-}
+/**
+  * 从搜索引擎过来的实战课程点击数实体类
+  * @param day_search_course
+  * @param click_count
+  */
+case class CourseSearchClickCount (day_search_course:String,click_count:Long)
